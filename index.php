2e8cdbbe3f38e66a9f5a977a1ee50a19687c9dce
Vinicius Bender <viniciusbender01@outlook.com>
2023-06-02 18:58:29 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Styles-->
+    <link type="text/css" rel="stylesheet" href="./styles/index.css">
+    <!--Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap"
+        rel="stylesheet">
+    <!--Icons-->
+    <script src="https://kit.fontawesome.com/8a0ec70a6e.js" crossorigin="anonymous"></script>
+    <!--Design https://museshopcart.webflow.io/-->
+    <title>SuplementaFit</title>
+</head>
+
+<body>
+
+    <header>
+        <div id="phone">
+            <i class="fa-solid fa-phone" style="color: #ffffff;"></i>
+            <p>+55 (55) 123456789</p>
+        </div>
+
+
+        <div class="logo">
+            <p id="espacamento">💪<p>
+            <p>SuplementaFit</p>
+            <p id="inverter">💪</p>
+        </div>
+
+        <div class="info">
+            <div class="cart">
+                <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+                <p>Cart</p>
+            </div>
+
+            <div class="account">
+                <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+                <p>Login</p>
+            </div>
+        </div>
+
+        <!-- <div id="texto">
+            <p>Os melhores preços do mercado!  <strong>|</strong>  Compre agora!</p>
+        </div>         -->
+
+        <!-- <div class="custom-select">
+            <i class="fa-solid fa-language" style="color: #ffffff;"></i>
+            <select >
+                <option >Idioma</option>
+                <option>Português</option>
+                <option>Enligsh</option>
+                <option>Espanõl</option>
+            </select>
+        </div> -->
+    </header>
+
+    <!-- <div class="menu">
+        <div class="logo">
+            <img src="./assets/images/mande_maromba-removebg-preview.png" width="60px">
+            <p>SuplementaFit</p>
+        </div>
+
+        <div class="info">
+            <div class="cart">
+                <i class="fa-solid fa-cart-shopping" style="color: #000000;"></i>
+                <p>Cart</p>
+            </div>
+
+            <div class="account">
+                <i class="fa-solid fa-user" style="color: #000000;"></i>
+                <p>Login</p>
+            </div>
+        </div>
+    </div> -->
+
+    <section class="produtos">
+
+    </section>
+
+
+</body>
+
+</html>
